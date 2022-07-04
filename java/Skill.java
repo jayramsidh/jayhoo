321f245b9c1bdc45c93112eacc86ceaec033720b
jayramsidh <sidhjayram@gmail.com>
2022-07-04 20:14:52 +0530

    kuch nhau

---
+++ java/Skill.java
@@ -0,0 +1,17 @@
+class Skill 
+{
+	public static void main(String [] args)
+	{
+		//Object o = new String();
+        //Skill d = new Skill();
+         // object typecasting 
+		//String s = (String) d; // 1.rule (c) & d should be same or must have parent-child child-parent type relations(error - inconvertale type )
+		                         //  required type - c & found type d
+
+		//String s1 = (Skill)o;  // 2. rule A nad c should be same or A must be parent and child must be child of it (incompatible type error found c and required A;
+        
+        //Skill s2 = (Skill)o;
+
+	}
+
+}
